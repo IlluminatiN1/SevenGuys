c0c73dc8cee95a0de016117a40681125d0324d2f
Emilio <emiliofaundes1@gmail.com>
2024-10-16 15:18:56 +0200

    added rootstack screens

--- Navigator/RootStackNavigator.tsx
+++ Navigator/RootStackNavigator.tsx
@@ -1,8 +1,21 @@
 import { NavigatorScreenParams } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React from "react";
+import ChoresScreen from "../Screen/ChoresScreen";
+import CreateHouseholdScreen from "../Screen/CreateHouseholdScreen";
+import EditChoreScreen from "../Screen/EditChoreScreen";
+import LogInScreen from "../Screen/LogInScreen";
+import ProfileScreen from "../Screen/ProfileScreen";
+import RegisterUserScreen from "../Screen/RegisterUserScreen";
 import TabNavigator, { TabParamList } from "./TabNavigator";
 
 export type RootStackParamList = {
+  Profile: undefined;
+  Chores: undefined;
+  LogIn: undefined;
+  RegisterUser: undefined;
+  EditChores: undefined;
+  CreateHousehold: undefined;
   HomeNavigator: NavigatorScreenParams<TabParamList>;
 };
 
@@ -11,6 +24,15 @@ export const RootStack = createNativeStackNavigator<RootStackParamList>();
 export default function RootStackNavigator() {
   return (
     <RootStack.Navigator>
+      <RootStack.Screen name="LogIn" component={LogInScreen} />
+      <RootStack.Screen name="Profile" component={ProfileScreen} />
+      <RootStack.Screen name="Chores" component={ChoresScreen} />
+      <RootStack.Screen name="RegisterUser" component={RegisterUserScreen} />
+      <RootStack.Screen name="EditChores" component={EditChoreScreen} />
+      <RootStack.Screen
+        name="CreateHousehold"
+        component={CreateHouseholdScreen}
+      />
       <RootStack.Screen name="HomeNavigator" component={TabNavigator} />
     </RootStack.Navigator>
   );
