09c5f32bd2e2010886d87abaafd5067ae59a58a1
Dankosalihi <danijelsalihi@hotmail.com>
2024-10-17 15:14:30 +0200

    setup redux and read initial state and added typed redux hooks

--- store/hooks.ts
+++ store/hooks.ts
@@ -1,15 +1,5 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
-import { store } from "./store";
-
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
-
-export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
-
-export const createAppAsyncThunk = createAsyncThunk.withTypes<{
-  state: RootState
-  dispatch: AppDispatch
-  rejectValue: string // använd denna för att skicka felmeddelanden "could not log in user"
-}>()
+import { useDispatch, useSelector } from 'react-redux'
+import type { AppDispatch, RootState } from './store'
+// Use throughout your app instead of plain `useDispatch` and `useSelector`
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
+export const useAppSelector = useSelector.withTypes<RootState>()
