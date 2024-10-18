c8d9b0413ec4bf9fdcf029df4784f75839376247
IlluminatiN1 <patriksvaerd@gmail.com>
2024-10-18 13:02:04 +0200

    added username and password validation

--- Screen/RegisterUserScreen.tsx
+++ Screen/RegisterUserScreen.tsx
@@ -1,13 +1,28 @@
 import * as React from "react";
 import { useState } from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import { Alert, Text, TouchableOpacity, View } from "react-native";
 import { Button, TextInput } from "react-native-paper";
 import { registerUserStyles } from "../styles";
+import { validatePassword } from "../utils/validations/PasswordValidator";
+import { validateUsername } from "../utils/validations/UsernameValidator";
 
 export default function SignUpScreen() {
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
-  //const [username, setUsername] = useState("");
-  //const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleRegisterUser = () => {
+    const usernameValidationMessage = validateUsername(username);
+    if (usernameValidationMessage !== "Username is valid") {
+      Alert.alert("Validation Error", usernameValidationMessage);
+      return;
+    }
+    const validationMessage = validatePassword(password);
+    if (validationMessage !== "Password is valid") {
+      Alert.alert("Validation Error", validationMessage);
+      return;
+    }
+  }
   //const dispatch = useAppDispatch(); // ska jag använda appdispatch eller ha en useRegisterDispatch?
 
   //const handleSignUp = async () => {
@@ -21,6 +36,8 @@ export default function SignUpScreen() {
         mode="outlined"
         label="Ange användarnamn"
         placeholder="Användarnamn"
+        value={username}
+        onChangeText={()}
         style={registerUserStyles.inputField}
       />
 
