8973b5df239ea470268b3a79d663a74c217d6b0c
Droxx98 <jooooooel@hotmail.se>
2024-10-21 13:57:59 +0200

    [WIP] Added actions for adding household + task

--- store/household/houseHoldAction.ts
+++ store/household/houseHoldAction.ts
@@ -1,2 +1,25 @@
-//  Definierar de olika typer av händelser som kan ske i 
-// applikationen, samt skapar actions som beskriver vad som har hänt.
+// store/household/householdActions.ts
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { db } from "../../config/firebase";
+import { doc, setDoc, collection, addDoc } from "firebase/firestore";
+
+type HouseholdPayload = { name: string; owner: string; members: string[] };
+
+export const createHousehold = createAsyncThunk(
+  "household/create",
+  async ({ name, owner, members }: HouseholdPayload, thunkAPI) => {
+    try {
+        console.log("Creating household");
+      const docRef = await addDoc(collection(db, "households"), {
+        name,
+        owner,
+        members,
+      });
+      console.log("Household created");
+      return { id: docRef.id, name, owner, members };
+    } catch (error) {
+      console.error("Error creating household:", error);
+      return thunkAPI.rejectWithValue("Could not create household");
+    }
+  }
+);
