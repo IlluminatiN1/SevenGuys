ebca51d66fa91cd7488dcf0143c61cbdfcfb5beb
Droxx98 <jooooooel@hotmail.se>
2024-10-21 12:34:00 +0200

    Added comment with the validation function

--- Screen/CreateHouseholdScreen.tsx
+++ Screen/CreateHouseholdScreen.tsx
@@ -1,8 +1,15 @@
 import { Text, View } from "react-native";
+import { validateHouseholdName } from "../utils/validations/household/HouseholdNameValidator";
 export default function CreateHouseholdScreen() {
+  
+  // Använd validateHouseholdName("name"); för att kontrollera att namnet på household är längre än 3 tecken.
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Create Household screen</Text>
+      
     </View>
+    
+    
   );
+  
 }
