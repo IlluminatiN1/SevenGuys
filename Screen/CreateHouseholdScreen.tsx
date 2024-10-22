fcbc88839dac7e26a7d5f8c5d6706742b62e8b67
97mirza <97mircoc@skola.boras.se>
2024-10-22 10:06:58 +0200

    checkbox function for chores added

--- Screen/CreateHouseholdScreen.tsx
+++ Screen/CreateHouseholdScreen.tsx
@@ -1,23 +1,32 @@
 import { useState } from "react";
 import { StyleSheet, TouchableOpacity, View } from "react-native";
-import { Button, Text, TextInput } from "react-native-paper";
+import { Button, Checkbox, Text, TextInput } from "react-native-paper";
 // Importera vår valideringsfunktion här för hushållsnamn (kommer implementeras senare)
 // import { validateHouseholdName } from "../utils/validations/household/HouseholdNameValidator";
 
 interface Chore {
   name: string;
+  checked: boolean;
 }
 
 export default function CreateHouseholdScreen() {
   const [householdName, setHouseholdName] = useState<string>("");
-  const [chores] = useState<Chore[]>([
-    { name: "Laga mat 🍳" },
-    { name: "Damma 🧹" },
-    { name: "Diska 🍽️" },
-    { name: "Ta hand om My 🐱" },
-    { name: "Torka golvet 🧼" },
-    { name: "Vattna blommor 🌸" },
+  const [chores, setChores] = useState<Chore[]>([
+    { name: "Laga mat 🍳", checked: false },
+    { name: "Damma 🧹", checked: false },
+    { name: "Diska 🍽️", checked: false },
+    { name: "Ta hand om My 🐱", checked: false },
+    { name: "Torka golvet 🧼", checked: false },
+    { name: "Vattna blommor 🌸", checked: false },
   ]);
+
+  const toggleChore = (index: number) => {
+    setChores(
+      chores.map((chore, i) =>
+        i === index ? { ...chore, checked: !chore.checked } : chore
+      )
+    );
+  };
 
   return (
     <View style={s.container}>
@@ -30,8 +39,14 @@ export default function CreateHouseholdScreen() {
 
       <Text>Sysslor:</Text>
       {chores.map((chore, index) => (
-        <TouchableOpacity key={index}>
+        <TouchableOpacity key={index} onPress={() => toggleChore(index)}>
           <Text>{chore.name}</Text>
+          <View>
+            <Checkbox
+              status={chore.checked ? "checked" : "unchecked"}
+              onPress={() => toggleChore(index)}
+            />
+          </View>
         </TouchableOpacity>
       ))}
       <Button>Spara</Button>
