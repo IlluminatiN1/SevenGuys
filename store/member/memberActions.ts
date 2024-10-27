616032d5dc02b4728a519510eb05f8995c898638
Droxx98 <jooooooel@hotmail.se>
2024-10-27 11:41:04 +0100

    Created members action

--- store/member/memberActions.ts
+++ store/member/memberActions.ts
@@ -1,2 +1,34 @@
-//  Definierar de olika typer av händelser som kan ske i 
-// applikationen, samt skapar actions som beskriver vad som har hänt.
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { db } from "../../config/firebase";
+import { collection, addDoc, doc, updateDoc } from "firebase/firestore";
+import { Member, mockedMembers } from "../../data/data";
+
+export const createMember = createAsyncThunk(
+  "member/createMember",
+  async (Member: Member, thunkAPI) => {
+    try {
+      const memberDocRef = await addDoc(collection(db, "members"), Member);
+      return { Member, id: memberDocRef.id };
+    } catch (error) {
+      console.error("Error creating member:", error);
+      return thunkAPI.rejectWithValue("Could not create member");
+    }
+  }
+);
+
+export const updateMemberEmoji = createAsyncThunk(
+  "member/updateMemberEmoji",
+  async ({ memberId, emojiId }: { memberId: number; emojiId: number, }, thunkAPI) => {
+    try {
+      const member = mockedMembers.find((m) => m.id === memberId);
+      if (member) {
+        member.emojiId = emojiId;
+      }
+      return { memberId, emojiId };
+    } catch (error) {
+      console.error("Error updating member emoji:", error);
+      return thunkAPI.rejectWithValue("Could not update member emoji");
+    }
+  }
+);
+
