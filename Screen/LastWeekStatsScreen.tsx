b7beeba622d9be8164f8cc89514b5cd02b5a6e34
IlluminatiN1 <patriksvaerd@gmail.com>
2024-10-29 15:09:24 +0100

    added basic total chartpie with mockeddata

--- Screen/LastWeekStatsScreen.tsx
+++ Screen/LastWeekStatsScreen.tsx
@@ -1,4 +1,7 @@
-import { Dimensions } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import React from "react";
+import { Dimensions, Text, View } from "react-native";
+import { PieChart } from "react-native-chart-kit";
 import {
   emojis,
   mockedCompletedTasks,
@@ -27,3 +30,46 @@ const calculateMemberScore = () => {
     };
   });
 };
+
+export default function LastWeekStatsScreen() {
+  const memberScores = calculateMemberScore();
+
+  const chartData = memberScores.map((member) => ({
+    name: member.name,
+    population: member.score,
+    color: member.color,
+    legendFontColor: "#7F7F7F",
+    legendFontSize: 12,
+  }));
+  return (
+    <View>
+      <Text>Hushållet</Text>
+      <Text>förra veckan</Text>
+
+      <PieChart
+      data={chartData}
+      width={screenWidth}
+      height={180}
+      chartConfig={{color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+    }}
+    accessor={"population"}
+    backgroundColor={"transparent"}
+    paddingLeft={"15"}
+    hasLegend={false}
+    center={[10, 10]}
+    />
+    <View>
+      {memberScores.map((member, index) => (
+        <MaterialCommunityIcons
+        key={index}
+        name={member.emojiName as keyof typeof MaterialCommunityIcons.glyphMap}
+        size={30}
+        color={member.color}
+        />
+      ))}
+    </View>
+    </View>
+  );
+}
+
+
