a963e67262f3ce05131d917e79cc2599017e4554
97mirza <97mircoc@skola.boras.se>
2024-10-30 20:06:30 +0100

    create task
    Co-authored-by: David Jensen <Tsourdox@users.noreply.github.com>
    Co-authored-by: Emilio Faundes <EmilioFau@users.noreply.github.com>
    Co-authored-by: Patrik Svärd <IlluminatiN1@users.noreply.github.com>
    Co-authored-by: Dankosalihi <Dankosalihi@users.noreply.github.com>

--- Screen/HouseholdScreen.tsx
+++ Screen/HouseholdScreen.tsx
@@ -2,7 +2,7 @@ import { MaterialCommunityIcons } from "@expo/vector-icons";
 import React, { useState } from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { IconButton } from "react-native-paper";
-import { CreateChorePopUpScreen } from "../components/CreateChoreComponent";
+import { CreateTaskPopUpScreen } from "../components/CreateChoreComponent";
 import {
   emojis,
   mockedCompletedTasks,
@@ -10,6 +10,7 @@ import {
   mockedTasks,
   mockedUser,
 } from "../data/data";
+import { useAppSelector } from "../store/hooks";
 
 const activeHousehold = "1";
 const activeTasks = mockedTasks.length > 0 ? mockedTasks : [];
@@ -28,6 +29,7 @@ export default function HouseholdScreen() {
   const [isModalVisible, setModalVisible] = useState(false);
   const showModal = () => setModalVisible(true);
   const hideModal = () => setModalVisible(false);
+  const tasks = useAppSelector(state => state.tasks.list);
 
   return (
     <View style={s.screenContainer}>
@@ -51,7 +53,7 @@ export default function HouseholdScreen() {
         />
       </View>
       <View>
-        {activeTasks
+        {tasks
           .filter((task) => task.householdId === activeHousehold)
           .map((task, index) => {
             return (
@@ -69,7 +71,7 @@ export default function HouseholdScreen() {
           <AddTaskButton onPress={showModal} />
         </View>
       </View>
-      {isModalVisible && <CreateChorePopUpScreen onClose={hideModal} />}
+      {isModalVisible && <CreateTaskPopUpScreen onClose={hideModal} />}
     </View>
   );
 }
