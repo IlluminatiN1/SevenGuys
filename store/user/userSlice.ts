c4b6986b940a38b0413e0991a8e5537f0f31e218
97mirza <97mircoc@skola.boras.se>
2025-04-27 16:03:35 +0200

    Add button to sign out

--- store/user/userSlice.ts
+++ store/user/userSlice.ts
@@ -1,6 +1,6 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { User } from "firebase/auth";
-import { signUpUser } from "./userActions";
+import { signUpUser, signOutUser } from "./userActions";
 
 interface UserState {
   user?: User;
@@ -13,17 +13,21 @@ const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
-    setUser(state, action: PayloadAction<User>) {
-      state.user = action.payload;
+    setUser(state, action: PayloadAction<User | null>) {
+      state.user = action.payload || undefined;
       state.error = undefined;
     },
   },
   extraReducers: (builder) => {
-    builder.addCase(signUpUser.rejected, (state, action) => {
-      state.error = action.payload as any;
-    })
-  }
+    builder
+      .addCase(signUpUser.rejected, (state, action) => {
+        state.error = action.payload as any;
+      })
+      .addCase(signOutUser.fulfilled, (state) => {
+        state.user = undefined;
+      });
+  },
 });
 
 export const { setUser } = userSlice.actions;
-export const userReducer =  userSlice.reducer;
+export const userReducer = userSlice.reducer;
