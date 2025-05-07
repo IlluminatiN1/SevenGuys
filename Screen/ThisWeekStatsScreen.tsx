edfdeb29105cc434063201d8eca33c1a89d1b6fc
IlluminatiN1 <patriksvaerd@gmail.com>
2025-05-07 22:59:09 +0200

    fixed so scroll function on individual stat

--- Screen/ThisWeekStatsScreen.tsx
+++ Screen/ThisWeekStatsScreen.tsx
@@ -1,13 +1,19 @@
 import React from "react";
-import { View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import ThisWeekTotalStatsComponent from "../components/stats/ThisWeekStatsComponent";
 import ThisWeekIndividualTaskStatComponent from "../components/stats/ThisWeekStatsIndividualComponent";
 
 export default function LastWeekStatsScreen() {
   return (
-    <View>
+    <View style={style.container}>
       <ThisWeekTotalStatsComponent />
       <ThisWeekIndividualTaskStatComponent />
     </View>
   );
 }
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
